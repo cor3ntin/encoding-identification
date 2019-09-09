7c58fef1c4b60d820517baa268e1513f68200b15
Corentin Jabot <corentinjabot@gmail.com>
2019-09-09 23:12:47 +0200

    Fix bugs and use the experimental GCC patch

--- encodings.hpp
+++ encodings.hpp
@@ -34,6 +34,7 @@ struct text_encoding {
 
 #include <algorithm>
 #include <locale>
+#include <cstring>
 #include <langinfo.h>
 #include "encodings_generated.hpp"
 
@@ -41,16 +42,16 @@ namespace cor3ntin::encoding {
 
 namespace details {
     constexpr bool compare_name(const char* a, const char* b) noexcept {
+        if(!a || !b)
+            return false;
         auto la = a;
         auto lb = b;
-        for(; *la != '\0', *lb != '\0'; la++, lb++) {
+        for(; *la != '\0' && *lb != '\0'; la++, lb++) {
             while(*la == '-' || *la == '_') {
                 la ++;
-                if(*la == '\0') return false;
             }
             while(*lb == '-' || *lb == '_') {
                 lb ++;
-                if(*lb == '\0') return false;
             }
             if(tolower(*la) != tolower(*lb))
                 return false;
@@ -134,18 +135,33 @@ private:
 
 
 struct text_encoding {
-    using id = details::id;
+            enum id {
+            other = 1,
+            unknown = 2,
+            ascii = 3,
+            latin1 = 4,
+            utf8 = 106,
+            ucs4 = 1001,
+            utf16be = 1013,
+            utf16le = 1014,
+            utf16 = 1015,
+            utf1632 = 1017,
+            reserved = 3000
+        };
 
-    /*constexpr*/ text_encoding() noexcept : mib_(2) {}
-    /*constexpr*/ text_encoding(std::string name, int mib = id::other) noexcept
-    : name_(std::move(name)), mib_(mib) {}
+    constexpr text_encoding() noexcept : mib_(2) {}
+    constexpr text_encoding(const char* name, int mib = id::other)
+    : mib_(mib) {
+        std::fill(std::begin(name_), std::end(name_), 0);
+        strncpy(std::begin(name_), name, std::size(name_) -1);
+    }
 
     constexpr int mib() const noexcept{
         return mib_;
     }
 
     const char* name() const noexcept{
-        return name_.c_str();
+        return name_.data();
     }
 
     details::encoding_alias_view aliases() const noexcept{
@@ -170,8 +186,9 @@ struct text_encoding {
 
 
 private:
-    std::string name_;
-    int mib_;
+    //poor man constexpr string
+    std::array<char, 30> name_ = {};
+    int mib_ = unknown;
 };
 
 
@@ -186,8 +203,13 @@ text_encoding text_encoding::current() {
 }
 
 text_encoding text_encoding::wide_current() {
+#ifdef WIN32
+    // windOWS is always UTF-16LE
+    return text_encoding("UTF-16LE", details::id::UTF16LE);
+#else
     // GLIBC is always UCS4
     return text_encoding("ISO-10646-UCS-4", details::id::UCS4);
+#endif
 }
 
 text_encoding text_encoding::for_locale(const std::locale& l) {
@@ -203,12 +225,26 @@ text_encoding text_encoding::wide_for_locale(const std::locale& l) {
 }
 
 text_encoding text_encoding::literal() {
+#ifdef __GXX_PRESUMED_EXECUTION_ENCODING
+    return text_encoding(__GXX_PRESUMED_EXECUTION_ENCODING,
+        details::find_encoding(__GXX_PRESUMED_EXECUTION_ENCODING));
+#else
     return text_encoding("UTF-8", details::id::UTF8);
+#endif
 }
 
 text_encoding text_encoding::wide_literal() {
-    return text_encoding("ISO-10646-UCS-4", details::id::UCS4);
+#ifdef WIN32
+    // windOWS is always UTF-16LE ?
+    return text_encoding("UTF-16LE", details::id::UTF16LE);
+#elif defined(__GXX_PRESUMED_WIDE_EXECUTION_ENCODING)
+    return text_encoding(__GXX_PRESUMED_WIDE_EXECUTION_ENCODING,
+        details::find_encoding(__GXX_PRESUMED_WIDE_EXECUTION_ENCODING));
+#else
+    return text_encoding("UTF-16", details::id::UTF16);
+#endif
 }
 
 
 }
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <encodings.hpp>
 #include <iostream>
 
-int main() {
-    //std::setlocale(LC_ALL, "");
-    auto c = cor3ntin::encoding::text_encoding::current();
-    std::cout << c.name() << "\n";
+void print(const cor3ntin::encoding::text_encoding & c) {
+    std::cout << c.name()
+             << " (iana mib: " << c.mib() << ")\n"
+             << "Aliases:\n";
     for(auto && a : c.aliases()) {
-        std::cout << a << '\n';
+        std::cout << '\t' << a << '\n';
     }
+}
+
+int main() {
+    std::cout << "Literal Encoding: ";
+    print(cor3ntin::encoding::text_encoding::literal());
+    std::cout << "Wide Literal Encoding: ";
+    print(cor3ntin::encoding::text_encoding::wide_literal());
+    std::cout << "Runtime Encoding: ";
+    print(cor3ntin::encoding::text_encoding::current());
+    std::cout << "Wide runtime Encoding: ";
+    print(cor3ntin::encoding::text_encoding::wide_current());
 }
